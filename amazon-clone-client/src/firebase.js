21dadbeeb82a80409f29fa5aec7474fdaec045f6
ggandara8 <ggandara8@gmail.com>
2020-10-01 22:02:01 -0500

    created navbar

---
+++ amazon-clone-client/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDucWTCSVOzb5lPYo7ozzZjqj0a4Tl0UbI",
+    authDomain: "clone-600d7.firebaseapp.com",
+    databaseURL: "https://clone-600d7.firebaseio.com",
+    projectId: "clone-600d7",
+    storageBucket: "clone-600d7.appspot.com",
+    messagingSenderId: "526481714652",
+    appId: "1:526481714652:web:0c2308b788135f39447319",
+    measurementId: "G-XYM53T9BTT"
+};
