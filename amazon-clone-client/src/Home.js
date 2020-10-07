0da76510cd4d0b0a7d9ae08524ffba45875f1d95
ggandara8 <ggandara8@gmail.com>
2020-10-06 22:44:12 -0500

    added products

--- amazon-clone-client/src/Home.js
+++ amazon-clone-client/src/Home.js
@@ -13,16 +13,52 @@ function Home() {
         />
 
         <div className="home_row">
-          <Product />
-          <Product />
+          <Product
+            id="1"
+            title="The Art of Resilience"
+            price={29.99}
+            image="https://m.media-amazon.com/images/I/51eK5eCWRyL.jpg"
+            rating={5}
+          />
+          <Product
+            id="2"
+            title="Can't Hurt Me"
+            price={19.99}
+            image="https://m.media-amazon.com/images/I/81eO3s9VJ4L.jpg"
+            rating={5}
+          />
         </div>
         <div className="home_row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id="3"
+            title="Living with a Seal"
+            price={19.99}
+            image="https://m.media-amazon.com/images/I/51EAJroqoWL.jpg"
+            rating={4}
+          />
+          <Product
+            id="4"
+            title="Breath"
+            price={14.99}
+            image="https://m.media-amazon.com/images/I/41o5B3nR1gL.jpg"
+            rating={5}
+          />
+          <Product
+            id="5"
+            title="The World's Fittest Book"
+            price={14.99}
+            image="https://m.media-amazon.com/images/I/51yr9sCjg6L._SX260_.jpg"
+            rating={5}
+          />
         </div>
         <div className="home_row">
-          <Product />
+          <Product
+            id="6"
+            title="Born to Run: A Hidden Tribe, Superathletes, and the Greatest Race the World Has Never Seen"
+            price={12.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/61KQDhUPv1L.jpg"
+            rating={4}
+          />
         </div>
       </div>
     </div>
--- amazon-clone-client/src/Product.js
+++ amazon-clone-client/src/Product.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product_info">
-        <p>The Art of Resilience by Ross Edgley</p>
+        <p>{title}</p>
         <p className="product_price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product_rating">
-          <p>⭐</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://m.media-amazon.com/images/I/51eK5eCWRyL.jpg"
-        alt="The Art of Resilience"
-      />
+      <img src={image} alt={title} />
       <button>Add to Basket</button>
     </div>
   );
