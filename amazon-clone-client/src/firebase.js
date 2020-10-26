126df497c5e66143fcbbc1e5a1f8856cb047cefb
ggandara8 <ggandara8@gmail.com>
2020-10-26 14:35:33 -0500

    deployed?

--- amazon-clone-client/src/firebase.js
+++ amazon-clone-client/src/firebase.js
@@ -1,11 +1,20 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDucWTCSVOzb5lPYo7ozzZjqj0a4Tl0UbI",
-    authDomain: "clone-600d7.firebaseapp.com",
-    databaseURL: "https://clone-600d7.firebaseio.com",
-    projectId: "clone-600d7",
-    storageBucket: "clone-600d7.appspot.com",
-    messagingSenderId: "526481714652",
-    appId: "1:526481714652:web:0c2308b788135f39447319",
-    measurementId: "G-XYM53T9BTT"
+  apiKey: "AIzaSyDucWTCSVOzb5lPYo7ozzZjqj0a4Tl0UbI",
+  authDomain: "clone-600d7.firebaseapp.com",
+  databaseURL: "https://clone-600d7.firebaseio.com",
+  projectId: "clone-600d7",
+  storageBucket: "clone-600d7.appspot.com",
+  messagingSenderId: "526481714652",
+  appId: "1:526481714652:web:0c2308b788135f39447319",
+  measurementId: "G-XYM53T9BTT",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
